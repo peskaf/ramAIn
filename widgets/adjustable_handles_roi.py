44a57f902435f16e14bef5b4e756dee9e7274ff5
Filip Peška <56216861+peskaf@users.noreply.github.com>
2022-05-03 20:29:06 +0200

    Adjustable handles docu comments added.

--- widgets/adjustable_handles_roi.py
+++ widgets/adjustable_handles_roi.py
@@ -1,7 +1,19 @@
 import pyqtgraph as pg
 
-# OK
+
 class AdjustableHandlesROI(pg.RectROI):
+    """
+    A class subclassing `pg.RectROI` so that handles are bigger.
+
+    Attributes:
+        handleSize (int): Size of the handles.
+    """
+
     def addHandle(self, *args, **kwargs):
+        """
+        Overrided function to add the handles to the ROI with desired `handleSize`.
+        Note the camel style for compatibility.
+        """
+
         self.handleSize = 10
         super(AdjustableHandlesROI, self).addHandle(*args, **kwargs)
