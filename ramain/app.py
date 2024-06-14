941ebad53248b52d1321e08f10a399c0704d6a4e
peskaf <filip@peska.me>
2024-06-14 17:46:01 +0200

    Registry fixed.

--- ramain/app.py
+++ ramain/app.py
@@ -6,6 +6,8 @@ from ramain.views.main_window import MainWindow
 class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
+        self.setOrganizationName("RamAIn")
+        self.setApplicationName("RamAIn")
         self.main_view = MainWindow()  # self.model, self.main_controller)
         self.main_view.show()
 
--- ramain/utils/settings.py
+++ ramain/utils/settings.py
@@ -1,3 +1,17 @@
+import os
 from PySide6.QtCore import QSettings
 
-SETTINGS = QSettings()
+SETTINGS = QSettings("RamAIn", "RamAIn")
+
+# NOTE: should not be needed as we now have set organization and app name
+if SETTINGS.value("spectral_map/cmap") is None:
+    SETTINGS.setValue("spectral_map/cmap", "hot")
+
+if SETTINGS.value("source_dir") is None:
+    SETTINGS.setValue("source_dir", os.getcwd())
+
+if SETTINGS.value("logs_dir") is None:
+    SETTINGS.setValue("logs_dir", os.getcwd())
+
+if SETTINGS.value("export_dir") is None:
+    SETTINGS.setValue("export_dir", os.getcwd())
